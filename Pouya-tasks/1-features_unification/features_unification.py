08fa1c1fb9573fce4331ebe553c1f7d8fc0ad1c7
Pouya Firouzmakan <pfirouzmakan@gmail.com>
2022-03-11 12:31:56 -0500

    modification

--- Pouya-tasks/1-features_unification/features_unification.py
+++ Pouya-tasks/1-features_unification/features_unification.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[94]:
 
 
 import numpy as np
 import pandas as pd
 import os
 import glob
+import ntpath
 
 
-# In[6]:
+# In[97]:
 
 
 def features_unification(datasets_address):
@@ -26,9 +27,50 @@ def features_unification(datasets_address):
 
     for i in csv_files:
         
+        ## Features Unification
+        
         data = pd.read_csv(i)
         data.columns = col
+        
+        ## Adding Age Column
+        if ntpath.basename(i)[0:2] == 'JC':
+            year = np.int(ntpath.basename(i)[3:7])
+        else:
+            year = np.int(ntpath.basename(i)[0:4])    
+        
+        
+        data.loc[data.loc[:,'usertype']=='Subscriber','age'] = year - data[data.loc[:,'usertype']=='Subscriber'].loc[:,'birth year'].astype('int')
+        
+        ## Export
         data.to_csv(i, index = False)
-    
-    
+
+
+# In[98]:
+
+
+features_unification('C:/Users/adria/OneDrive/Desktop')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
